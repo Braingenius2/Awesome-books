8a5e9930643152c854f03fe751226310cd863359
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-05-03 20:00:04 +0300

    Add the class for the object and use method to add book

--- script.js
+++ script.js
@@ -1,51 +1,130 @@
-// define books as a collection of arrays
-let books = [
-  {
-    id: 1,
-    title: 'Lorem',
-    author: 'Testeroo Testyy',
-  },
-  {
-    id: 2,
-    title: 'Ipsum',
-    author: 'Testeroo Testyy',
-  },
-  {
-    id: 3,
-    title: 'Dolor',
-    author: 'Testeroo Testyy',
-  },
-];
+// // define books as a collection of arrays
+// let books = [
+//   {
+//     id: 1,
+//     title: 'Lorem',
+//     author: 'Testeroo Testyy',
+//   },
+//   {
+//     id: 2,
+//     title: 'Ipsum',
+//     author: 'Testeroo Testyy',
+//   },
+//   {
+//     id: 3,
+//     title: 'Dolor',
+//     author: 'Testeroo Testyy',
+//   },
+// ];
 
-// Get DOM elements
+// // Get DOM elements
+// const bookListElement = document.getElementById('book-list');
+// const addButtonElement = document.getElementById('add-btn');
+// const titleInputElement = document.getElementById('title-input');
+// const authorInputElement = document.getElementById('author-input');
+
+// function addBook(title, author) {
+//   const book = {
+//     id: books.length + 1,
+//     title,
+//     author,
+//   };
+//   books.push(book);
+//   localStorage.setItem('books', JSON.stringify(books));
+// }
+
+// function removeBook(id) {
+//   books = books.filter((book) => book.id !== id);
+//   let localStorageBooks = JSON.parse(localStorage.getItem('books'));
+//   localStorageBooks = localStorageBooks.filter((obj) => obj.id !== parseInt(id, 10));
+//   localStorage.setItem('books', JSON.stringify(localStorageBooks));
+// }
+
+// function displayBookList() {
+//   // Clear the book list element
+//   bookListElement.innerHTML = '';
+
+//   // Loop through the book collection and create a new element for each book
+//   books.forEach((book) => {
+//     const bookElement = document.createElement('div');
+//     bookElement.innerHTML = `<p><span class="title">${book.title}</span><br>
+//     <span class="author">${book.author}</span></p>`;
+//     bookElement.id = `book-${book.id}`;
+//     const removeButton = document.createElement('button');
+//     removeButton.textContent = 'Remove';
+//     removeButton.dataset.id = book.id;
+//     removeButton.addEventListener('click', (event) => {
+//       const { id } = event.target.dataset;
+//       removeBook(id);
+//       const bookElement = document.getElementById(`book-${id}`);
+//       bookListElement.removeChild(bookElement);
+//     });
+//     bookElement.appendChild(removeButton);
+//     bookListElement.appendChild(bookElement);
+//   });
+// }
+
+// // Check if there is any data in localStorage and load it
+// if (localStorage.getItem('books')) {
+//   books = JSON.parse(localStorage.getItem('books'));
+// }
+
+// // Event listener for the add book button
+// addButtonElement.addEventListener('click', (event) => {
+//   event.preventDefault();
+
+//   // Get the values from the input fields
+//   const title = titleInputElement.value;
+//   const author = authorInputElement.value;
+
+//   // Add the book to the collection
+//   addBook(title, author);
+
+//   // Clear the input fields
+//   titleInputElement.value = '';
+//   authorInputElement.value = '';
+
+//   // Display the updated book list
+//   displayBookList();
+// });
+
+// displayBookList();
+
 const bookListElement = document.getElementById('book-list');
 const addButtonElement = document.getElementById('add-btn');
 const titleInputElement = document.getElementById('title-input');
 const authorInputElement = document.getElementById('author-input');
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.books = JSON.parse(localStorage.getItem('books') || '[]');
+    this.id = Math.random();
+  }
 
-function addBook(title, author) {
-  const book = {
-    id: books.length + 1,
-    title,
-    author,
-  };
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
+  addBook() {
+    const book = {
+      id: Math.random(),
+      title: this.title,
+      author: this.author,
+    };
+    this.books.push(book);
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  removeBook(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+    let localStorageBooks = JSON.parse(localStorage.getItem('books'));
+    localStorageBooks = localStorageBooks.filter((book) => book.id !== parseInt(id, 10));
+    localStorage.setItem('books', JSON.stringify(localStorageBooks));
+  }
 }
-
-function removeBook(id) {
-  books = books.filter((book) => book.id !== id);
-  let localStorageBooks = JSON.parse(localStorage.getItem('books'));
-  localStorageBooks = localStorageBooks.filter((obj) => obj.id !== parseInt(id, 10));
-  localStorage.setItem('books', JSON.stringify(localStorageBooks));
-}
-
 function displayBookList() {
   // Clear the book list element
   bookListElement.innerHTML = '';
-
+  const displObj = new Books();
   // Loop through the book collection and create a new element for each book
-  books.forEach((book) => {
+  displObj.books.forEach((book) => {
     const bookElement = document.createElement('div');
     bookElement.innerHTML = `<p><span class="title">${book.title}</span><br><span class="author">${book.author}</span></p>`;
     bookElement.id = `book-${book.id}`;
@@ -54,7 +133,7 @@ function displayBookList() {
     removeButton.dataset.id = book.id;
     removeButton.addEventListener('click', (event) => {
       const { id } = event.target.dataset;
-      removeBook(id);
+      displObj.removeBook(id);
       const bookElement = document.getElementById(`book-${id}`);
       bookListElement.removeChild(bookElement);
     });
@@ -62,22 +141,16 @@ function displayBookList() {
     bookListElement.appendChild(bookElement);
   });
 }
-
-// Check if there is any data in localStorage and load it
-if (localStorage.getItem('books')) {
-  books = JSON.parse(localStorage.getItem('books'));
-}
-
 // Event listener for the add book button
 addButtonElement.addEventListener('click', (event) => {
   event.preventDefault();
-
+  const addObject = new Books();
   // Get the values from the input fields
-  const title = titleInputElement.value;
-  const author = authorInputElement.value;
+  addObject.title = titleInputElement.value;
+  addObject.author = authorInputElement.value;
 
   // Add the book to the collection
-  addBook(title, author);
+  addObject.addBook();
 
   // Clear the input fields
   titleInputElement.value = '';
@@ -86,5 +159,3 @@ addButtonElement.addEventListener('click', (event) => {
   // Display the updated book list
   displayBookList();
 });
-
-displayBookList();
