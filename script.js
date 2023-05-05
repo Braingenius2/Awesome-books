49c6b045a455a70caa11fc9133f33a2716bb3186
Braingenius2 <braingenius2@gmail.com>
2023-05-05 03:28:40 +0300

    Add click event listeners to nav links

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const bookListElement = document.getElementById('book-list');
 const addButtonElement = document.getElementById('add-btn');
 const titleInputElement = document.getElementById('title-input');
 const authorInputElement = document.getElementById('author-input');
-const dateTime=document.querySelector('.date-time')
+const listLink = document.querySelector('.list-link');
+const addLink = document.querySelector('.add-link');
+const contactLink = document.querySelector('.contact-link');
+const formElement = document.getElementById('form');
+const contactElement = document.getElementById('contact');
+const dateTime=document.querySelector('.date-time');
 
 let today = new Date();
 let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -79,3 +84,22 @@ addButtonElement.addEventListener('click', (event) => {
   // Display the updated book list
   booksObj.displayBookList();
 });
+
+// Event listeners for links on nav menu
+listLink.addEventListener('click', () => {
+  bookListElement.style.display = 'flex'
+  formElement.style.display = 'none';
+  contactElement.style.display = 'none';
+})
+
+addLink.addEventListener('click', () => {
+  bookListElement.style.display = 'none';
+  formElement.style.display = 'flex';
+  contactElement.style.display = 'none';
+})
+
+contactLink.addEventListener('click', () => {
+  bookListElement.style.display = 'none';
+  formElement.style.display = 'none';
+  contactElement.style.display = 'flex';
+})
